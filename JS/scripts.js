2bbc07053ddc2c3d85984383d55e6c2acda965af
Cricky <crickmonster@gmail.com>
2021-02-16 19:59:47 -0500

    JS- event listeners

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,8 @@ const todoInput = document.querySelector(".todo_input");
 const todoButton = document.querySelector(".todo_button");
 const todoList = document.querySelector(".todo_list");
 const filterOption = document.querySelector(".filter_todo");
+
+//event listeners
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
