16f58ffe3612c70927551f65f0e6f11f91b09095
Cricky <crickmonster@gmail.com>
2021-02-16 19:59:11 -0500

    JS- Selectors

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+//selectors
+const todoInput = document.querySelector(".todo_input");
+const todoButton = document.querySelector(".todo_button");
+const todoList = document.querySelector(".todo_list");
+const filterOption = document.querySelector(".filter_todo");
