94ecc6d53cf39cce41cb028a5c6cb9eeb3914e51
Cricky <crickmonster@gmail.com>
2021-02-16 20:50:06 -0500

    JS- fixed error- was missing a bracket

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,81 +9,81 @@ todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
 filterOption.addEventListener("click", filterTodo);
 
-
 //Add Check Button & Delete Button
 //functions
 function addTodo(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-//todo DIV
-const todoDiv = document.createElement('div');
-todoDiv.classList.add('todo');
+  //todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
 
-//todo LI 
-const newTodo = document.createElement('li');
-newTodo.innerText = todoInput.value;
-newTodo.classList.add('todo_item');
-todoDiv.appendChild(newTodo);
-if(todoInput.value === ""){
-    return null
-}
+  //todo LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo_item");
+  todoDiv.appendChild(newTodo);
+  if (todoInput.value === "") {
+    return null;
+  }
 
-//check BUTTON
-const completedButton = document.createElement('button');
-completedButton.innerHTML = '<i class="fas fa-check"></i>';
-completedButton.classList.add('complete_btn')
-todoDiv.appendChild(completedButton);
+  //check BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete_btn");
+  todoDiv.appendChild(completedButton);
 
- //delete BUTTON
- const deleteButton = document.createElement('button');
- deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
- deleteButton.classList.add('delete_btn')
- todoDiv.appendChild(deleteButton);
- //Append to Actual LIST
- todoList.appendChild(todoDiv);
- //Clear todo input VALUE
- todoInput.value = ""
+  //delete BUTTON
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+  deleteButton.classList.add("delete_btn");
+  todoDiv.appendChild(deleteButton);
+  //Append to Actual LIST
+  todoList.appendChild(todoDiv);
+  //Clear todo input VALUE
+  todoInput.value = "";
 
- //DELETE & CHECK
-function deleteCheck(e) {
+  //DELETE & CHECK
+  function deleteCheck(e) {
     const item = e.target;
     //DELETE ITEM
     if (item.classList[0] === "delete_btn") {
-        const todo = item.parentElement;
-        //ANIMATION TRANSITION
-        todo.classList.add("fall")
-        todo.addEventListener('transitionend', function () {
-            todo.remove()
-        })
+      const todo = item.parentElement;
+      //ANIMATION TRANSITION
+      todo.classList.add("fall");
+      todo.addEventListener("transitionend", function () {
+        todo.remove();
+      });
     }
     //TASK COMPLETE
     if (item.classList[0] === "complete_btn") {
-        const todo = item.parentElement;
-        todo.classList.toggle("completedItem")
+      const todo = item.parentElement;
+      todo.classList.toggle("completedItem");
     }
-}
-//FILTER TASKS
-function filterTodo(e) {
+  }
+  //FILTER TASKS
+  function filterTodo(e) {
     const todos = todoList.childNodes;
-    for(let i = 1; i<todos.length; i++ ){
-        switch (e.target.value) {
-            case "all":
-                todos[i].style.display = "flex";
-                break;
-            case "completed":
-                if (todos[i].classList.contains('completedItem')) {
-                    todos[i].style.display = "flex";
-                } else {
-                    todos[i].style.display = "none";
-                }
-                break;
-            case "uncompleted":
-                if (!todos[i].classList.contains('completedItem')) {
-                    todos[i].style.display = "flex";
-                } else {
-                    todos[i].style.display = "none";
-                }
-                break;
-        }
+    for (let i = 1; i < todos.length; i++) {
+      switch (e.target.value) {
+        case "all":
+          todos[i].style.display = "flex";
+          break;
+        case "completed":
+          if (todos[i].classList.contains("completedItem")) {
+            todos[i].style.display = "flex";
+          } else {
+            todos[i].style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todos[i].classList.contains("completedItem")) {
+            todos[i].style.display = "flex";
+          } else {
+            todos[i].style.display = "none";
+          }
+          break;
+      }
     }
-} 
+  }
+}
